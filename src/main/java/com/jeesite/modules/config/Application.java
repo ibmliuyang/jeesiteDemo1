2c2dfaa9d51813c3d52284786e87adb27fe52773
liuyang <393670@qq.com>
2018-11-18 17:56:20 +0800

    提交-a

--- src/main/java/com/jeesite/modules/config/Application.java
+++ src/main/java/com/jeesite/modules/config/Application.java
@@ -24,7 +24,8 @@ public class Application extends SpringBootServletInitializer {
 		app.setDefaultProperties(PropertiesUtils.getInstance().getProperties());
 		app.run(args);
 	}
-	
+
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		this.setRegisterErrorPageFilter(false); // 错误页面有容器来处理，而不是SpringBoot
